8278324373e385eb7dcc2f8976f234b853a36d7c
roboruss <144238368+Pdiceneged@users.noreply.github.com>
2024-09-05 09:15:17 -0300

    finacer.py

--- aloras/finacer.py
+++ aloras/finacer.py
@@ -1,34 +1,40 @@
+from flask import Flask, render_template, request
 import base64
-import streamlit as st
 from joblib import load
 
+# Inicializar o aplicativo Flask
+app = Flask(__name__)
+
 # Carregar o modelo, o vetorizador e o label encoder
+model = load('random_forest_model.joblib')
+vectorizer = load('vectorizer.joblib')
+label_encoder = load('label_encoder.joblib')
 
-vectorizer = load('aloras/vectorizer.joblib')
-label_encoder = load('aloras/label_encoder.joblib')
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
 
+img = get_img_as_base64("pdipaper5.png")
+img2 = get_img_as_base64("pdipaper5.png")
 
+# Rota principal
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    prediction = None
+    if request.method == 'POST':
+        user_input = request.form['user_input']
+        # Vetorizar o input do usuário
+        user_input_transformed = vectorizer.transform([user_input])
 
+        # Fazer a predição
+        prediction_encoded = model.predict(user_input_transformed)
 
-# Código de estilo e imagem omitido para simplificação
+        # Converter a predição codificada de volta para o nome da categoria
+        prediction = label_encoder.inverse_transform(prediction_encoded)[0]
 
-# Título do aplicativo
-st.title('Análise de Suprimentos (Financeiro)')
+    return render_template('index.html', img=img, img2=img2, prediction=prediction)
 
-# Input do usuário
-user_input = st.text_input("Digite o Histórico:")
-
-# Botão para realizar a predição
-if st.button('Prever Natureza'):
-    # Vetorizar o input do usuário
-    user_input_transformed = vectorizer.transform([user_input])
-
-
-    # Converter a predição codificada de volta para o nome da categoria
-    prediction = label_encoder.inverse_transform(prediction_encoded)
-
-    # Exibir a predição
-    st.write(f'**A Natureza prevista é:** {prediction[0]}')
-
-st.sidebar.markdown("---")
-st.sidebar.markdown("Desenvolvido por [PedroFS](https://linktr.ee/Pedrofsf)")
+# Iniciar o servidor
+if __name__ == '__main__':
+    app.run(debug=True)
